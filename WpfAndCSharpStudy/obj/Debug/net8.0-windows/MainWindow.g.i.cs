72f9a189cb94829b55095c3e7d98756b2379fa68
yhan <yhan@visionsemicon.co.kr>
2025-04-10 13:20:52 +0900

    Update

--- WpfAndCSharpStudy/obj/Debug/net8.0-windows/MainWindow.g.i.cs
+++ WpfAndCSharpStudy/obj/Debug/net8.0-windows/MainWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace WpfAndCSharpStudy {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WpfAndCSharpStudy;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WpfAndCSharpStudy;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
